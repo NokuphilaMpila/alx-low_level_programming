cd44c93200b107389d26a330f529203a0f1a19df
NokuphilaMpila <njabulonokuphila96@gmail.com>
2023-09-08 08:51:02 -0700

    Task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,48 +13,20 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf = NULL;
-	ssize_t fd, w, t;
-
-	if (filename == NULL)
-	{
-		return (0);
-	}
+	char *buf;
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		perror("open");
 		return (0);
-	}
-
 	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-	{
-		perror("malloc");
-		close(fd);
-		return (0);
-	}
-
 	t = read(fd, buf, letters);
-	if (t == -1)
-	{
-		perror("read");
-		free(buf);
-		close(fd);
-		return (0);
-	}
-
 	w = write(STDOUT_FILENO, buf, t);
-	if (w == -1 || w != t)
-	{
-		perror("write");
-		free(buf);
-		close(fd);
-		return (0);
-	}
 
 	free(buf);
 	close(fd);
 	return (w);
 }
+
